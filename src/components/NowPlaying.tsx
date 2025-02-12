08a9b1edae3fddfc328fa2a0e8afc93f9639f9a4
heymynameisrob <robhough180@gmail.com>
2025-02-12 14:15:01 +0000

    better now playing

--- src/components/NowPlaying.tsx
+++ src/components/NowPlaying.tsx
@@ -3,59 +3,74 @@ import { AnimatePresence, motion } from "motion/react";
 import { fetcher } from "@/lib/fetch";
 
 import type { Track } from "@/lib/types";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/primitives/HoverCard";
+import { ChevronRightIcon } from "@heroicons/react/16/solid";
 
 export function NowPlaying() {
-  const { data } = useSWR<Track>("/api/now-playing", fetcher);
+  const { data } = useSWR<Track>("/api/now-playing", fetcher, {
+    refreshInterval: 30_000, // 30 seconds
+  });
 
   if (!data) return null;
 
-  const image = data.image.find((img) => img.size === "medium")?.url;
+  const image = data.image.find((img) => img.size === "extralarge")?.url;
+
+  if (!data.nowPlaying) return null;
 
   return (
-    <AnimatePresence mode="popLayout">
-      {data.nowPlaying ? (
-        <motion.div
-          initial={{ y: 20, filter: "blur(4px)", opacity: 0 }}
+    <div className="flex items-baseline gap-1 -mx-2 animate-in fade-in fill-mode-both duration-300 overflow-y-clip">
+      <Icon />
+      <AnimatePresence mode="popLayout" initial={false}>
+        <motion.span
+          key={data.url}
+          initial={{ y: 10, filter: "blur(2px)", opacity: 0 }}
           animate={{ y: 0, filter: "blur(0px)", opacity: 1 }}
-          exit={{ y: 20, filter: "blur(4px)", opacity: 0 }}
-          className="fixed bottom-8 left-0 right-0 grid place-items-center z-50"
+          exit={{ y: 10, filter: "blur(2px)", opacity: 0 }}
+          transition={{ type: "spring", bounce: 0, duration: 0.2 }}
         >
-          <div className="flex justify-between items-center gap-4 w-96 bg-gray-1 border rounded-xl p-2 shadow-lg dark:shadow-2xl relative after:absolute after:inset-0 after:rounded-xl after:bg-gradient-to-r dark:after:from-white/5 after:to-transparent after:pointer-events-none">
-            <a
-              href={data.url}
-              target="_blank"
-              rel="noopener nofollow"
-              className="flex w-full items-center gap-2"
+          <HoverCard>
+            <HoverCardTrigger aria-disabled={!image}>
+              <a
+                href={data.url}
+                target="_blank"
+                rel="noopener nofollow"
+                className="flex w-full items-center gap-2 hover:opacity-70"
+              >
+                <p className="text-sm font-medium text-primary truncate">
+                  {data.name}
+                </p>
+                <ChevronRightIcon className="w-3 h-3 opacity-60" />
+                <p className="text-sm text-secondary truncate">{data.artist}</p>
+              </a>
+            </HoverCardTrigger>
+            <HoverCardContent
+              align="start"
+              className="aspect-square p-0 rounded-lg overflow-clip data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 duration-200"
             >
               <img
                 src={image}
                 alt={data.album}
                 loading="lazy"
-                className="aspect-square size-12 rounded-md"
+                className="object-fit w-full h-full"
               />
-              <div className="flex flex-col">
-                <p className="text-sm font-medium text-primary truncate">
-                  {data.name}
-                </p>
-                <p className="font-mono uppercase text-sm text-secondary truncate">
-                  {data.artist}
-                </p>
-              </div>
-            </a>
-            <Icon />
-          </div>
-        </motion.div>
-      ) : null}
-    </AnimatePresence>
+            </HoverCardContent>
+          </HoverCard>
+        </motion.span>
+      </AnimatePresence>
+    </div>
   );
 }
 
 function Icon() {
   return (
-    <div className="grid items-center px-3">
-      <span className="relative flex items-center justify-between size-5 mask-linear mask-from-100 mask-to-50">
+    <div className="grid items-center px-3 overlflow-clip">
+      <span className="relative flex items-center justify-between size-4">
         <motion.span
-          className="w-[5px] h-full bg-cyan-400 origin-bottom rounded-t-[1px]"
+          className="w-[4px] h-full bg-cyan-400 origin-bottom rounded-t-[1px]"
           animate={{
             scaleY: [0.3, 1, 0.5, 0.75, 0.5],
           }}
@@ -67,7 +82,7 @@ function Icon() {
           }}
         />
         <motion.span
-          className="w-[5px] h-full bg-cyan-400 origin-bottom rounded-t-[1px]"
+          className="w-[4px] h-full bg-cyan-400 origin-bottom rounded-t-[1px]"
           animate={{
             scaleY: [0.3, 1, 0.5, 0.75, 0.5],
           }}
@@ -80,7 +95,7 @@ function Icon() {
           }}
         />
         <motion.span
-          className="w-[5px] h-full bg-cyan-400 origin-bottom rounded-t-[1px]"
+          className="w-[4px] h-full bg-cyan-400 origin-bottom rounded-t-[1px]"
           animate={{
             scaleY: [0.3, 1, 0.5, 0.75, 0.5],
           }}
