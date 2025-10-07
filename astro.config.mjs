d872a2cc78f3058790cfa9b0f49712f687a29e50
heymynameisrob <robhough180@gmail.com>
2025-10-07 10:17:54 +0100

    move to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,15 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: getBaseUrl(),
   output: "static",
-  adapter: vercel(),
+  adapter: netlify(),
   markdown: {
     shikiConfig: {
       theme: "andromeeda",
