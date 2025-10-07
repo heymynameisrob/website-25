1fe5a7c64612e3f8ffbbbf8046c0e3f6698ecb30
heymynameisrob <robhough180@gmail.com>
2025-10-07 10:33:01 +0100

    back to static lol

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   site: getBaseUrl(),
-  output: "hybrid",
+  output: "static",
   adapter: netlify(),
   markdown: {
     shikiConfig: {
