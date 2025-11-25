97a5364f272460a18f4bffc8807755fe7abda5c5
heymynameisrob <robhough180@gmail.com>
2025-11-25 16:57:42 +0000

    refactored

--- src/components/demos/Gallery.tsx
+++ src/components/demos/Gallery.tsx
@@ -1,6 +1,13 @@
 import * as React from "react";
-import { AnimatePresence, motion } from "motion/react";
+import {
+  AnimatePresence,
+  LayoutGroup,
+  motion,
+  usePresence,
+} from "motion/react";
 import { cn } from "@/lib/utils";
+import { AspectRatio } from "@radix-ui/react-aspect-ratio";
+import { useClickAway } from "@uidotdev/usehooks";
 
 type Image = {
   url: string;
@@ -35,152 +42,53 @@ const IMAGES: Image[] = [
 ];
 
 export function Gallery() {
-  const [currentImage, setCurrentImage] = React.useState<string | null>(null);
-  const [transitionType, setTransitionType] =
-    React.useState<TransitionType>("open");
-
-  const handleImageOpen = (image: string) => {
-    setTransitionType("open");
-    setCurrentImage(image);
-  };
-
-  /** Stop scrolling when image dialog is open */
-  React.useEffect(() => {
-    document.body.style.overflow = currentImage ? "hidden" : "unset";
-
-    return () => {
-      document.body.style.overflow = "unset";
-    };
-  }, [currentImage]);
-
-  return (
-    <div className="not-prose w-full max-w-md mx-auto">
-      <div className="w-full grid grid-cols-2 gap-4">
-        {IMAGES.map((img) => (
-          <motion.button
-            key={img.url}
-            layoutId={img.url}
-            whileHover={{ opacity: 0.8 }}
-            className="relative aspect-square rounded-2xl bg-gray-2 overflow-hidden border focus transition-all"
-            onClick={() => handleImageOpen(img.url)}
-          >
-            <img
-              src={img.url}
-              className="absolute inset-0 object-cover"
-              alt="Preview image"
-            />
-          </motion.button>
-        ))}
-      </div>
-
-      <GalleryDialog
-        image={currentImage}
-        onImageChange={setCurrentImage}
-        transitionType={transitionType}
-        setTransitionType={setTransitionType}
-      />
-    </div>
-  );
-}
-
-function GalleryDialog({
-  image,
-  onImageChange,
-  setTransitionType,
-  transitionType,
-}: {
-  image: string | null;
-  onImageChange: React.Dispatch<React.SetStateAction<string | null>>;
-  transitionType: TransitionType;
-  setTransitionType: React.Dispatch<React.SetStateAction<TransitionType>>;
-}) {
-  const next = React.useCallback(() => {
-    if (!image) return;
-
-    const index = IMAGES.findIndex((p) => p.url === image);
-    if (index === IMAGES.length - 1) return;
-
-    setTransitionType("navigate");
-    onImageChange(IMAGES[index + 1].url);
-  }, [image, onImageChange, setTransitionType]);
-
-  const prev = React.useCallback(() => {
-    if (!image) return;
-
-    const index = IMAGES.findIndex((p) => p.url === image);
-    if (index === 0) return;
-
-    setTransitionType("navigate");
-    onImageChange(IMAGES[index - 1].url);
-  }, [image, onImageChange, setTransitionType]);
+  const [selectedImage, setSelectedImage] = React.useState<Image | null>(null);
+  const ref = useClickAway<HTMLDivElement>(() => setSelectedImage(null));
 
   React.useEffect(() => {
-    const handleKeydown = (event: KeyboardEvent) => {
+    const onKeyDown = (event: KeyboardEvent) => {
       if (event.key === "Escape") {
-        event.preventDefault();
-        setTransitionType("open");
-        onImageChange(null);
-      }
-      if (event.key === "ArrowLeft") {
-        event.preventDefault();
-        if (event.metaKey || event.ctrlKey) {
-          setTransitionType("navigate");
-          onImageChange(IMAGES[0].url);
-        } else {
-          prev();
-        }
-      }
-      if (event.key === "ArrowRight") {
-        event.preventDefault();
-        if (event.metaKey || event.ctrlKey) {
-          setTransitionType("navigate");
-          onImageChange(IMAGES[IMAGES.length - 1].url);
-        } else {
-          next();
-        }
+        setSelectedImage(null);
       }
     };
 
-    window.addEventListener("keydown", handleKeydown);
-    return () => window.removeEventListener("keydown", handleKeydown);
-  }, [next, prev, onImageChange, transitionType, setTransitionType]);
+    window.addEventListener("keydown", onKeyDown);
+    return () => window.removeEventListener("keydown", onKeyDown);
+  }, []);
 
   return (
     <>
-      <AnimatePresence>
-        {image ? (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            onClick={() => {
-              setTransitionType("open");
-              onImageChange(null);
-            }}
-            className="absolute inset-0 z-10 backdrop-blur-sm"
-          />
-        ) : null}
-      </AnimatePresence>
-
-      <AnimatePresence>
-        {image ? (
-          <div className="absolute inset-10 grid place-items-center z-10 pointer-events-none">
-            <motion.div
-              key={image}
-              layoutId={image}
-              className={cn(
-                "relative aspect-square w-full max-w-md rounded-3xl shadow-2xl overflow-hidden",
-              )}
-            >
-              <img
-                src={image}
-                className="absolute inset-0 object-cover"
-                alt="Expanded image" // Adding alt for accessibility
-              />
-            </motion.div>
-          </div>
-        ) : null}
-      </AnimatePresence>
+      {selectedImage && <ExpandedImage ref={ref} image={selectedImage} />}
+      <ul className="flex flex-col lg:grid lg:grid-cols-2 gap-2">
+        {IMAGES.map((image) => (
+          <button
+            key={image.url}
+            onClick={() => setSelectedImage(image)}
+            className="aspect-square h-[200px] rounded-2xl hover:opacity-80 cursor-pointer overflow-hidden outline-none focus transition-all"
+          >
+            <img src={image.url} alt={image.caption} loading="eager" />
+          </button>
+        ))}
+      </ul>
     </>
   );
 }
+
+const ExpandedImage = React.forwardRef<HTMLDivElement, { image: Image }>(
+  ({ image }, ref) => {
+    return (
+      <>
+        <div className="absolute inset-0 bg-background/50 backdrop-blur-md z-10 pointer-events-none" />
+        <div className="absolute inset-4 grid place-items-center z-50">
+          <div
+            ref={ref}
+            className="aspect-square h-[400px] rounded-2xl cursor-pointer overflow-hidden outline-none focus transition-all"
+          >
+            <img src={image.url} alt={image.caption} loading="eager" />
+          </div>
+        </div>
+      </>
+    );
+  },
+);
+ExpandedImage.displayName = "ExpandedImage";
