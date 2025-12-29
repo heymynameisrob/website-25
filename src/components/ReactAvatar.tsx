a2c42afe71f1b7e47e634fca6d041d34c888920c
heymynameisrob <robhough180@gmail.com>
2025-12-29 16:27:28 +0000

    added n8n role

--- src/components/ReactAvatar.tsx
+++ src/components/ReactAvatar.tsx
@@ -4,14 +4,10 @@ import {
   HoverCardTrigger,
 } from "@/components/primitives/HoverCard";
 
-export function ReactAvatar() {
+export function ReactAvatar({ children }: { children: React.ReactNode }) {
   return (
     <HoverCard openDelay={150}>
-      <HoverCardTrigger className="w-fit">
-        <h1 className="text-[22px] leading-tight tracking-tighter font-serif cursor-default">
-          Rob Hough
-        </h1>
-      </HoverCardTrigger>
+      <HoverCardTrigger className="w-fit">{children}</HoverCardTrigger>
       <HoverCardContent
         align="start"
         className="aspect-square p-0 rounded-lg overflow-clip data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 duration-200"
@@ -19,8 +15,8 @@ export function ReactAvatar() {
       >
         <img
           src="https://ucarecdn.com/75709875-783d-47e9-a60a-6d43e1d5d344/"
-          loading="lazy"
           alt="Rob Hough"
+          loading="eager"
           className="object-fit w-full h-full"
         />
       </HoverCardContent>
